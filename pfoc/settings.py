c07185a1ffdafb4fb90bccb1a6ae1531171b2ccf
Brux <mikael198@mail.ru>
2021-11-12 21:10:41 +0300

    change settings.py

--- pfoc/settings.py
+++ pfoc/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # DATABASES = {
 #     'default': {
