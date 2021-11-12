9931f204417a50d9700857166f95a09dd4f7a19b
Brux <mikael198@mail.ru>
2021-11-12 14:43:54 +0300

    update change data

--- pfoc/settings.py
+++ pfoc/settings.py
@@ -121,9 +121,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIR = [
+os.path.join(BASE_DIR, 'static')
+]
+
+#SMTP Configuration
+#
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "*****"
+# EMAIL_HOST_PASSWORD = "*****"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
