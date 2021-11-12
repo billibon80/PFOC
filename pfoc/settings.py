187da01672370e0b2c9e76d63b6071f1b870dc60
Brux <mikael198@mail.ru>
2021-11-12 21:36:38 +0300

    change settings.py

--- pfoc/settings.py
+++ pfoc/settings.py
@@ -137,14 +137,14 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # SMTP Configuration
 #
