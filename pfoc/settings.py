3dea7fc8b422f54ec05b3af33d3b7a8d8bcae068
Brux <mikael198@mail.ru>
2021-11-18 01:36:03 +0300

    apdate DATABASE Heroku

--- pfoc/settings.py
+++ pfoc/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'pfoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': config('SQL_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': config('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': config("SQL_USER", "user"),
-        'PASSWORD': config("SQL_PASSWORD", "password"),
-        'HOST': config("SQL_HOST", "DATABASE_URL"),
-        'PORT': config("SQL_PORT", "5432"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('SQL_ENGINE', 'django.db.backends.sqlite3'),
+#         'NAME': config('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+#         'USER': config("SQL_USER", "user"),
+#         'PASSWORD': config("SQL_PASSWORD", "password"),
+#         'HOST': config("SQL_HOST", "DATABASE_URL"),
+#         'PORT': config("SQL_PORT", "5432"),
+#     }
+# }
 
 DATABASE_URL = config('DATABASE_URL')
 
