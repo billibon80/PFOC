23a031c72f0c5cbf07b061cceaa34e6d849e920f
Brux <mikael198@mail.ru>
2021-11-18 21:56:13 +0300

    update runtime python to 3.9.9

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
-
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        try:
-            CustomUser.objects.get(username=username)
-        except CustomUser.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
--- homepage/models.py
+++ homepage/models.py
@@ -2,13 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-
-class CustomUser(models.Model):
-    username = models.CharField(max_length=30)
-    subscribe_newsletters = models.BooleanField(default=True)
-    old_id = models.IntegerField(null=True, blank=True)
-    old_source = models.CharField(max_length=25, null=True, blank=True)
-
-    class Meta:
-        db_table = 'pfoc'
-        app_label = 'frcstats'
