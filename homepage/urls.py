2577bfc1684f1902288b8b15ed8a7f2af94fba8c
Mikhail <mikael198@mail.ru>
2021-12-14 02:07:28 +0300

    First create pattern for PFOC

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,8 +7,5 @@ app_name = "homepage"
 urlpatterns = [
     path('', views.index, name='index'),
     path("home/", views.index, name="home"),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
-    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+
 ]
