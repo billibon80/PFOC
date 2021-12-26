aa31dcae52020ab9f4a1c6ce94970b997a670365
Mikhail <mikael198@mail.ru>
2021-12-26 19:54:22 +0300

    create calc calories and tabs views sport

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,51 +1,12 @@
+import tabs from './modules/tabs';
+import calc from './modules/calc';
 
 window.addEventListener("DOMContentLoaded", () => {
+      
+    tabs();
+    calc();
+
     
-     // Tabs & News Block
-         
-    function hideTabContent(blockContent, blockTab, selector) {
-        blockContent.forEach(item => {
-           // item.style.display = 'none'; // in-line style
-           item.classList.add('hide');
-           item.classList.remove('show');
-        });
-
-        blockTab.forEach(item => {
-            item.classList.remove(selector);
-        });
-    }
-
-    function showTabContent(i = 0, blockContent, blockTab, selector) {
-        blockContent[i].classList.add('show', 'fade');
-        blockContent[i].classList.remove('hide');
-        blockTab[i].classList.add(selector);
-
-    }
-
-    // Tabs
-
-    const tabsParent = document.querySelector('.tabheader__items'),
-          tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          selector = 'tabheader__item_active';
-
-
-    hideTabContent(tabsContent, tabs, selector);
-    showTabContent(0, tabsContent, tabs, selector);
-
-    tabsParent.addEventListener('click', (e) => {
-        const target = e.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                
-                if (target == item) {
-                    hideTabContent(tabsContent, tabs, selector);
-                    showTabContent(i, tabsContent, tabs, selector);
-                }
-            });
-        }
-    });
 
     
     // News
@@ -170,8 +131,6 @@ window.addEventListener("DOMContentLoaded", () => {
             allHash[i].scrollIntoView({block: 'center', behavior: 'smooth'});
         });
     });
-    console.log(allLinks);
-    console.log(allHash);
     
          
 });
