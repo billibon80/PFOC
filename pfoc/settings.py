aaefb8ec8f644f1b6e83103be8c78233054d9b7a
Mikhail <mikael198@mail.ru>
2022-03-14 23:33:58 +0300

    debug false

--- pfoc/settings.py
+++ pfoc/settings.py
@@ -106,16 +106,16 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('SQL_ENGINE', 'django.db.backends.sqlite3'),
-#         'NAME': config('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-#         'USER': config("SQL_USER", "user"),
-#         'PASSWORD': config("SQL_PASSWORD", "password"),
-#         'HOST': config("SQL_HOST", "DATABASE_URL"),
-#         'PORT': config("SQL_PORT", "5432"),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': config('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': config('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': config("SQL_USER", "user"),
+        'PASSWORD': config("SQL_PASSWORD", "password"),
+        'HOST': config("SQL_HOST", "DATABASE_URL"),
+        'PORT': config("SQL_PORT", "5432"),
+    }
+}
 
 # DATABASE_URL = config('DATABASE_URL')
 #
